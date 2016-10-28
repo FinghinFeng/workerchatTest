77e1bc67269f6e46fcc3c4dd81e336336d0e5055
FinghinFeng <okko1236987@gmail.com>
2016-10-28 15:54:10 +0800

    A workerchatTest project
    Test

---
+++ Applications/Chat/Config/Db.php
@@ -0,0 +1,21 @@
+<?php
+namespace Config;
+
+/**
+ *mysql配置
+ *@author finghin
+ */
+class Db
+{
+
+  public static $workerchat = array(
+      'host' => '127.0.0.1',
+      'port' => 3306,
+      'user' => 'root',
+      'password' => '123456',
+      'dbname' => 'workerchat',
+      'charset' => 'utf8',
+  );
+}
+
+?>
